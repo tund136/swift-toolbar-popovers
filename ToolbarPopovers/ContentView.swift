93c9a4624ae7766c10a5b5d5ac29858ce801ea9d
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-14 11:36:01 +0700

    Create the project

---
+++ ToolbarPopovers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToolbarPopovers
+//
+//  Created by Danh Tu on 14/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToolbarPopovers/ToolbarPopoversApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToolbarPopoversApp.swift
+//  ToolbarPopovers
+//
+//  Created by Danh Tu on 14/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct ToolbarPopoversApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
